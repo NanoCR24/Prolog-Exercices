5b5d0df2ae36e53a90ac3123a0e9e1ced0b8c8b1
JeffersonSR <91024572+NanoCR24@users.noreply.github.com>
2024-11-10 23:37:37 -0600

    Update problema 1.pl

--- problema 1.pl
+++ problema 1.pl
@@ -1,3 +1,10 @@
+% Integrantes
+% 305490093 - Alonso Usaga Bonilla
+% 118760067 - Juan Pablo Cartín Esquivel
+% 118780391 - Jefferson Sandí Ramírez
+% EIF400 – Paradigmas de Programación – NRC 50989
+% 2024-11-10 Proyecto #2 – Ejercicio #2.
+
 subsecuencia(X, X1) :- nonvar(X), var(X1), !, X=X1.  
 subsecuencia([], _).
 subsecuencia([X|T], [X|T2]) :- subsecuencia(T, T2).
